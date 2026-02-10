9dcf75c446f2e257cabbd5c268ffb63b56f90a24
Arnab Pal <arnab.i@gmail.com>
2026-02-10 18:30:41 +0530

    fix

--- app/src/main/java/com/example/marsphotos/data/MarsPhotosRepository.kt
+++ app/src/main/java/com/example/marsphotos/data/MarsPhotosRepository.kt
@@ -21,13 +21,4 @@ class NetworkMarsPhotosRepository(
     }.catch { throwable ->
         emit(DataResult.Error(throwable))
     }
-	
-	override fun getMarsPhotosById(id): Flow<DataResult<List<MarsPhoto>>> = flow {
-        emit(DataResult.Loading)
-
-        val photos = marsApiService.getPhotos(id)
-        emit(DataResult.Success(photos))
-    }.catch { throwable ->
-        emit(DataResult.Error(throwable))
-    }
 }
